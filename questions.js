e49496c7a2c74c5dcb018d2d84f611cef93c992e
Sharpc0der <86218142+Sharpc0der@users.noreply.github.com>
2021-07-08 12:42:50 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,230 @@
+
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+
+    {
+
+    numb: 1,
+
+    question: "C Language was developed in the year ____?",
+
+    answer: "1970",
+
+    options: [
+
+      "1970",
+
+      "1975",
+
+      "1980",
+
+      "1985",
+
+    ]
+
+  },
+
+    {
+
+    numb: 2,
+
+    question: " What is the 16-bit compiler allowable range for integer constants?",
+
+    answer: "-32768 to 32767",
+
+    options: [
+
+      "-3.4e38 to 3.4e38",
+
+      "-32767 to 32768",
+
+      "-32668 to 32667",
+
+      "-32768 to 32767"
+
+    ]
+
+  },
+
+    {
+
+    numb: 3,
+
+    question: "What is required in each C program?",
+
+    answer: "The program must have at least one function.",
+
+    options: [
+
+ "The program must have at least one function.",
+
+"The program does not require any function.",
+
+"Input data",
+
+"Output data",
+
+    ]
+
+  },
+
+    {
+
+    numb: 4,
+
+    question: "Which one is not a reserve keyword in C Language?",
+
+    answer: "main",
+
+    options: [
+
+      "auto",
+
+      "case",
+
+      "main",
+
+      "resister"
+
+    ]
+
+  },
+
+    {
+
+    numb: 5,
+
+    question: "Prototype of a function means?",
+
+    answer: "Declaration of Function",
+
+    options: [
+
+      "Name of Function",
+
+      "Output of Function",
+
+      "Declaration of Function",
+
+      "Input of Function",
+
+    ]
+
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  {
+
+   numb: 6,
+
+   question: "Name the loop that executes at least once",
+
+    answer: "do-while",
+
+   options: [
+
+   "while ",
+
+   "do-while",
+
+   "if-else",
+
+   "for"
+
+    ]
+
+  },
+
+    {
+
+   numb: 7,
+
+   question: "Far pointer can access ___",
+
+    answer: "All Memory Location",
+
+   options: [
+
+   "Single Memory Location",
+
+   "No Memory Location",
+
+   "All Memory Location",
+
+   "Frist And Last Memory Address"
+
+    ]
+
+  },
+
+  {
+
+   numb: 8,
+
+   question: "An uninitialized pointer in C is called ___",
+
+    answer: "Wild Pointer",
+
+   options: [
+
+   "Constructor",
+
+   "Dangling Pointer",
+
+   "Wild Pointer",
+
+   "Destructor"
+
+    ]
+
+  },
+
+  {
+
+   numb: 9,
+
+   question: "A pointer that is pointing to NOTHING is called ____",
+
+    answer: "NULL Pointer",
+
+   options: [
+
+   "VOID Pointer",
+
+   "Dangling Pointer",
+
+   "NULL Pointer",
+
+   "WILD pointer"
+
+    ]
+
+  },
+
+    {
+
+   numb: 10,
+
+   question: "What is a format specifier in C language.?",
+
+    answer: "All the above",
+
+   options: [
+
+   "A format Specifier tells compiler to treat a variable value is predefined way.",
+
+   "Different format specifiers are used to print different type of data. ",
+
+   "Format specifiers are used to write data to files in a formatted way.",
+
+   "All the above"
+
+    ]
+
+  },
+
+];
